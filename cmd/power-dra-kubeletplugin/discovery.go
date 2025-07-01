6642852dec93b121f60bc87150b15fb8a310175c
Paul Bastide <pbastide@us.ibm.com>
2025-07-01 15:43:36 -0400

    OpenShiftP-281: only one device is visible to the lpar, accounting for that with the capacity
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- cmd/power-dra-kubeletplugin/discovery.go
+++ cmd/power-dra-kubeletplugin/discovery.go
@@ -6,70 +6,55 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
 	"os"
 
-	"github.com/google/uuid"
 	resourceapi "k8s.io/api/resource/v1beta1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/klog/v2"
 	"k8s.io/utils/ptr"
 )
 
-const NXGZIPCAPS = "/host-sys/devices/vio/ibm,compression-v1/nx_gzip_caps"
+const CapsLocation = "/host-sys/devices/vio/ibm,compression-v1/nx_gzip_caps"
+const UpperLimit = "200"
+const Uuid = "b2ccae49-efdd-4d90-bc8e-6fec4a2b19f7"
 
 func enumerateAllPossibleDevices(numNx int) (AllocatableDevices, error) {
-	seed := os.Getenv("NODE_NAME")
-	uuids := generateUUIDs(seed, numNx)
 	alldevices := make(AllocatableDevices)
+
+	// If nx-gzip doesn't exist, then don't return any devices.
 	if !existsNxGzip() {
-		// If nx-gzip doesn't exist, then don't return any devices.
 		return alldevices, nil
 	}
 
-	for i, uuid := range uuids {
-		device := resourceapi.Device{
-			Name: fmt.Sprintf("nx-%d", i),
-			Basic: &resourceapi.BasicDevice{
-				Attributes: map[resourceapi.QualifiedName]resourceapi.DeviceAttribute{
-					"index": {
-						IntValue: ptr.To(int64(i)),
-					},
-					"uuid": {
-						StringValue: ptr.To(uuid),
-					},
-					"model": {
-						StringValue: ptr.To("LATEST-NX-MODEL"),
-					},
-					"driverVersion": {
-						VersionValue: ptr.To("0.1.0"),
-					},
+	// prepopulate
+
+	device := resourceapi.Device{
+		Name: "crypto/nx-gzip",
+		Basic: &resourceapi.BasicDevice{
+			Attributes: map[resourceapi.QualifiedName]resourceapi.DeviceAttribute{
+				"index": {
+					IntValue: ptr.To(int64(0)),
 				},
-				Capacity: map[resourceapi.QualifiedName]resourceapi.DeviceCapacity{
-					"memory": {
-						Value: resource.MustParse("80Gi"),
-					},
+				"uuid": {
+					StringValue: ptr.To(Uuid),
+				},
+				"model": {
+					StringValue: ptr.To("LATEST-NX-MODEL"),
+				},
+				"driverVersion": {
+					VersionValue: ptr.To("0.1.0"),
 				},
 			},
-		}
-		alldevices[device.Name] = device
+			Capacity: map[resourceapi.QualifiedName]resourceapi.DeviceCapacity{
+				"nx-gzip": {
+					Value: resource.MustParse(UpperLimit),
+				},
+			},
+		},
 	}
+	alldevices[device.Name] = device
+
 	return alldevices, nil
-}
-
-func generateUUIDs(seed string, count int) []string {
-	rand := rand.New(rand.NewSource(hash(seed)))
-
-	uuids := make([]string, count)
-	for i := 0; i < count; i++ {
-		charset := make([]byte, 16)
-		rand.Read(charset)
-		uuid, _ := uuid.FromBytes(charset)
-		uuids[i] = "nx-" + uuid.String()
-	}
-
-	return uuids
 }
 
 func hash(s string) int64 {
@@ -82,7 +67,7 @@ func hash(s string) int64 {
 
 // Detect NXGZIPCAPS exists
 func existsNxGzip() bool {
-	_, err := os.Stat(NXGZIPCAPS)
+	_, err := os.Stat(CapsLocation)
 	if err != nil {
 		klog.V(5).ErrorS(err, "Failed to detect Nest Accelerator nx-gzip feature")
 		return false
